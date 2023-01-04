5b4cb2f418cb99ed5a0544c488e5b4a7be52f4bf
TheRockNo04 <therockno04@gmail.com>
2023-01-05 00:21:05 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,9 +1,24 @@
 from tkinter import *
+import Roll
+import os
 
 root = Tk()
 
-myLabel = Label(root, text="Hello!")
+e = Entry(root)
 
-myLabel.pack()
+
+def sayHi():
+    greet = Label(root, text="Hello " + str(e) + "!")
+    greet.pack()
+
+button1 = Button(root, text="Click Me!", command=sayHi)
+button1.grid(row=0, column=0)
+
+
+def openxl():
+    os.startfile("Hi.txt")
+
+button2 = Button(root, text="Open file!", command=openxl)
+button2.grid(row=0, column=1)
 
 root.mainloop()
