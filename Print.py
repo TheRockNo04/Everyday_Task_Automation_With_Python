ae8bbbbffb28856447bcb8f07b03fa519fad75dc
TheRockNo04 <therockno04@gmail.com>
2023-01-05 00:11:00 +0530

    Update Print.py

--- Print.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import os
-
-DATE = datetime.now()
-
-while True:
-    a = int(input("Enter RollNo:- "))
-    for _ in range(3):
-        os.startfile(f"D:\\Piyush\\Roll\\{DATE.year}\\{DATE.month}\\{a}.xlsx", "print")
